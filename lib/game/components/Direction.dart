be4c26cdfb756336a0ff509cce60780ceae8bbaf
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-03-17 18:01:00 -0400

    Make determination based on which component of delta in 2-space is larger

--- lib/game/components/Direction.dart
+++ lib/game/components/Direction.dart
@@ -1,4 +1,5 @@
 import 'dart:ui';
+import 'dart:core';
 
 enum Direction {
   up,
@@ -11,18 +12,25 @@ class Directions {
 
   static Direction of({from: Rect, to: Rect}) {
 
-    if(from.left > to.left) {
-      return Direction.left;
+    double deltaX = (from.left - to.left).abs();
+    double deltaY = (from.top - to.top).abs();
+
+    if(deltaX - deltaY > 0) {         //  The x-component is larger than the y-component of the difference between points
+      if(from.left > to.left) {
+        return Direction.left;
+      }
+      if(from.left < to.left) {
+        return Direction.right;
+      }
+    } else if (deltaX - deltaY < 0) { //  The y-component is larger than the x-component of the differences between points
+      if(from.top < to.top) {
+        return Direction.up;
+      }
+      if(from.top > to.top) {
+        return Direction.down;
+      }
     }
-    if(from.left < to.left) {
-      return Direction.right;
-    }
-    if(from.top < to.top) {
-      return Direction.up;
-    }
-    if(from.top > to.top) {
-      return Direction.down;
-    }
+
 
     return null;
   }
