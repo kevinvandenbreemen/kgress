4a0a5f458ff8292625ef320cd7d5d4a55e388eb9
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-03-17 10:03:00 -0400

    Initial commit of the POC

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MainApp());
+
+class MainApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoApp(
+      theme: CupertinoThemeData(
+        barBackgroundColor: CupertinoColors.darkBackgroundGray,
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: CupertinoColors.darkBackgroundGray,
+        textTheme: CupertinoTextThemeData(
+          primaryColor: Colors.orange,
+          textStyle: TextStyle(
+            color: Colors.orangeAccent
+          )
+        )
+      ),
+      home: GameScaffold(),
+    );
+  }
+}
+
+class GameScaffold extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        backgroundColor: CupertinoColors.black,
+        actionsForegroundColor: CupertinoColors.extraLightBackgroundGray,
+        trailing: CupertinoButton(
+          child: Icon(CupertinoIcons.book_solid, color: Colors.orangeAccent,),
+          onPressed: ()=>debugPrint("GOt pressed"),
+        )
+
+      ),
+      child: GameWorld(),
+    );
+  }
+  
+}
+
+class GameWorld extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Game to Come");
+  }
+}
