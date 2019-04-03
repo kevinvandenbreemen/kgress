7702d0e8e1c53283504d201c25de787e2c4d7889
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-04-02 20:09:21 -0400

    General algorithm definition for updating each element and then drawing it.

--- lib/game/controller/element_controllers.dart
+++ lib/game/controller/element_controllers.dart
@@ -1,21 +1,28 @@
 import 'dart:ui';
 
 import 'package:kevin_gamify/game/elements/element.dart';
+import 'package:kevin_gamify/game/imagesets/state_image_set_providers.dart';
 
 abstract class ElementController {
 
   Element _element;
 
+  StateImagesetProvider _imageSetProvider;
+
   /// Update the element.  Returns the [Rect] corresponding to the element's
   /// current position in the game world, NOT necessarily its current position
   /// on the screen.
   Rect update(double timePassedSeconds) {
-    return onUpdate(timePassedSeconds, _element);
+    Rect ret = onUpdate(timePassedSeconds, _element);
+    _imageSetProvider.update(_element.state, timePassedSeconds);
+    return ret;
   }
 
   Rect onUpdate(double timePassedSeconds, Element element);
 
   /// Draw the element on the screen into the given rectangle
-  void draw(Rect rect, Paint paint, Canvas canvas);
+  void draw(Rect rect, Paint paint, Canvas canvas) {
+    _imageSetProvider.drawNextFrame(rect, paint, canvas);
+  }
 
 }
--- lib/game/imagesets/state_image_set_providers.dart
+++ lib/game/imagesets/state_image_set_providers.dart
@@ -1,10 +1,14 @@
 import 'dart:ui';
 
+import 'package:kevin_gamify/game/states/states.dart';
+
 /// Provides appropriate next im
 abstract class StateImagesetProvider {
 
+  /// Draw the next frame in the animation
   void drawNextFrame(Rect characterRect, Paint paint, Canvas canvas);
 
-  void update(double timePassedSeconds);
+  /// Update the provider with the current state and with how much time has passed
+  void update(State state, double timePassedSeconds);
 
 }
