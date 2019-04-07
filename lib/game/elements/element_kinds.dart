eb45c09061f6815c6ad74d5588cd69c61b4e879b
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-04-07 11:01:30 -0400

    Start work on stateful / animated image sets and their corresponding element kinds

--- lib/game/elements/element_kinds.dart
+++ lib/game/elements/element_kinds.dart
@@ -2,6 +2,7 @@ import 'package:kevin_gamify/game/elements/element_types.dart';
 import 'package:kevin_gamify/game/imagesets/image_set.dart';
 import 'package:kevin_gamify/game/states/state_spaces.dart' as stateSpaces;
 import 'package:kevin_gamify/game/states/states.dart' as states;
+import 'package:kevin_gamify/game/states/states.dart';
 
 /// Kind of element.  For example, a 'chair' is a KIND of 'stationary object'
 abstract class ElementKind {
@@ -39,5 +40,29 @@ class StationaryObjectKind extends ElementKind {
   @override
   Map<states.State, ImageSet> get statesToImageSets => {states.stationary: _imageSet};
 
+
+}
+
+/// Anything or anyone that can be in different states!
+class StatefulObjectKind extends ElementKind {
+
+  Map<states.State, ImageSet> _statesToImageSets;
+
+  StatefulObjectKind(ElementType type, stateSpaces.StateSpace stateSpace, {Map<states.State, ImageSet> statesToImageSets}) : super(type, stateSpace) {
+    if(statesToImageSets == null){
+      throw ArgumentError.notNull("statesToImageSets");
+    }
+
+    stateSpace.states.forEach((state) {
+      if(!statesToImageSets.keys.contains(state)){
+        throw ArgumentError("Missing image set for ${state}");
+      }
+    });
+
+    _statesToImageSets = statesToImageSets;
+  }
+
+  @override
+  Map<states.State, ImageSet> get statesToImageSets => Map.unmodifiable(_statesToImageSets);
 
 }
--- lib/game/imagesets/image_set.dart
+++ lib/game/imagesets/image_set.dart
@@ -3,10 +3,37 @@ abstract class ImageSet  {
 
 }
 
+/// Single image (for example, a floor tile, a desk, etc)
 class SingleImage extends ImageSet {
   String _imagePath;
   String get imagePath => _imagePath;
 
   SingleImage(this._imagePath);
+
+}
+
+/// A collection of images from a row on a sprite sheet for use in building an animations
+class SpriteSheetRowSequence extends ImageSet {
+
+  /*
+      return Animation.sequenced(imagePath,
+        numFrames,
+        textureHeight: height,
+        textureWidth: width,
+        textureX: 0.0,
+        textureY: sheetRow * height
+    );
+   */
+  final String imagePath;
+  final double textureHeight;
+  final double textureWidth;
+  final double textureX;
+  final double textureY;
+  final int sheetRow;
+  final int numFrames;
+
+  SpriteSheetRowSequence(this.imagePath, this.textureHeight, this.textureWidth,
+      this.textureX, this.textureY, this.sheetRow, this.numFrames);
+
 
 }
