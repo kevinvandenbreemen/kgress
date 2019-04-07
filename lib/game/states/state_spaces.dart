c74c9442213864793ddbf056a952cd0adc66723d
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-04-07 10:50:43 -0400

    Directional state space

--- lib/game/states/state_spaces.dart
+++ lib/game/states/state_spaces.dart
@@ -11,3 +11,14 @@ class StateSpace {
 
 /// States for a stationary, unmoving object
 StateSpace stationary = StateSpace([States.stationary]);
+
+/// Set of states for something that moves in the four compass directions.  It is not
+/// recommended that you use this space for a player character as it will prevent other
+/// states (happy, sad, dead, etc) from being used!
+StateSpace directional = StateSpace([
+  States.stationary,
+  States.movingUp,
+  States.movingDown,
+  States.movingLeft,
+  States.movingRight
+]);
