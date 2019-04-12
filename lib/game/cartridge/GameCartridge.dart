ea9ce37d82557cba33d6116c66ccf1c1a073f3d4
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-04-12 18:58:13 -0400

    Remove the concept of game editing

--- lib/game/cartridge/GameCartridge.dart
+++ lib/game/cartridge/GameCartridge.dart
@@ -14,24 +14,6 @@ class GameCartridge {
 
 }
 
-/// Type for editing a game cartridge.  IMPORTANT:  This type should not provide any
-/// business logic.  Its sole purpose is to provide access to game elements that a
-/// regular game player would not have access to on her own.
-class GameCartridgeForEdit {
-
-  GameCartridge _game;
-
-  GameCartridgeForEdit(this._game);
-
-  /// All areas in the game
-  List<Area> get areas => _game._areas;
-
-  void addArea(Area area) {
-    _game._areas.add(area);
-  }
-
-}
-
 class GameSettings {
 
   double tileWidthsPerScreen = 5;
