cfd3209268f57b4678f41dcb37a499ec504ac225
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-05-08 19:58:42 -0400

    Basic view

---
+++ lib/game/tools/ElementKindsToolsView.dart
@@ -0,0 +1,8 @@
+import 'package:kevin_gamify/game/elements/element_kinds.dart';
+
+mixin ElementKindsToolsView {
+
+  /// Set the element kinds that can be selected on this screen
+  void setElementKinds(List<ElementKind> kinds);
+
+}
