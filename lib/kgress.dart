91cfd88f476d308b132855de55e5a627c712fd77
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-08-20 21:56:25 -0400

    Public API and changelog/publication stuff

---
+++ lib/kgress.dart
@@ -0,0 +1,13 @@
+library kgress;
+
+export 'package:kgress/example/drawers/ExampleElementDrawerRepository.dart';
+export 'package:kgress/game/actions/action.dart';
+export 'package:kgress/game/actions/builtin/control_actions.dart';
+export 'package:kgress/game/actions/builtin/element_actions/action_move.dart';
+export 'package:kgress/game/actions/builtin/events.dart';
+export 'package:kgress/game/areas/model/Area.dart';
+export 'package:kgress/game/cartridge/GameCartridge.dart';
+export 'package:kgress/game/components/Direction.dart';
+export 'package:kgress/game/controller/element_controllers_repository_default.dart';
+export 'package:kgress/game/elements/element.dart';
+export 'package:kgress/game/world/game_world.dart';
