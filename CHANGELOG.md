3144dc6bd9a59a9fe7c33f800326209770059c88
Kevin VanDenBreemen <kevin.vandenbreemen@gmail.com>
2019-08-21 20:37:10 -0400

    Allow access to image sets/element types

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,6 @@
 ## 0.0.1
 Initial published version
+
+## 0.0.10
+* Update exposed libraries to include element kinds
+* Expose image sets
--- lib/kgress.dart
+++ lib/kgress.dart
@@ -1,14 +1,17 @@
+/// The KGRESS game engine for building simple 2d retro games with Dart
 library kgress;
 
-export 'package:kgress/example/drawers/ExampleElementDrawerRepository.dart';
-export 'package:kgress/game/actions/action.dart';
-export 'package:kgress/game/actions/builtin/control_actions.dart';
-export 'package:kgress/game/actions/builtin/element_actions/action_move.dart';
-export 'package:kgress/game/actions/builtin/events.dart';
-export 'package:kgress/game/areas/model/Area.dart';
-export 'package:kgress/game/cartridge/GameCartridge.dart';
-export 'package:kgress/game/components/Direction.dart';
-export 'package:kgress/game/controller/element_controllers_repository_default.dart';
-export 'package:kgress/game/elements/element.dart';
-export 'package:kgress/game/tools/cupertino_game_tool.dart';
-export 'package:kgress/game/world/game_world.dart';
+export 'example/drawers/ExampleElementDrawerRepository.dart';
+export 'game/actions/action.dart';
+export 'game/actions/builtin/control_actions.dart';
+export 'game/actions/builtin/element_actions/action_move.dart';
+export 'game/actions/builtin/events.dart';
+export 'game/areas/model/Area.dart';
+export 'game/cartridge/GameCartridge.dart';
+export 'game/components/Direction.dart';
+export 'game/controller/element_controllers_repository_default.dart';
+export 'game/elements/element.dart';
+export 'game/elements/element_kinds.dart';
+export 'game/imagesets/image_set.dart';
+export 'game/tools/cupertino_game_tool.dart';
+export 'game/world/game_world.dart';
